f69c0323e3bbe1fb28fbc3f48c72f5d90b8a2bb2
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2022-03-05 03:37:07 +0500

    Update NN.py

--- NN.py
+++ NN.py
@@ -1,45 +1,52 @@
-#this is dont work now
-
-from statistics import mode
 import eel
 import numpy as np
 from tensorflow import keras
 from keras.layers import Dense, Flatten
 from keras.datasets import mnist
 from PIL import Image
+import matplotlib.pyplot as plt
 
-eel.init('Web')
-
-
-def format(pixels):
-    print(11111)
-    print(pixels)
-    pixels = np.array(pixels)
-    pixels.shape = (392, 392)
-    img = Image.fromarray(pixels, mode='L')
-    img = img.resize((28, 28))
-    return img
+eel.init('D:/prog/indproject/Web')
 
 
-def learning(model):
+@eel.expose
+def go(data):
+    def formating(pixels):
+        pixels = np.array(pixels)
+        pixels.shape = (392, 392)
+        plt.imshow(pixels)
+        plt.show()
+
+        #Ниже проблема с конвертацие изображения
+        #Выдаёт ошибку на 22 строке
+        pixels = pixels.resize((28, 28))
+        print(pixels)
+        print(type(pixels))
+        plt.imshow(pixels)
+        plt.show()
+        return pixels
+
+    img = formating(data)
+    plt.imshow(img)
+    plt.show()
+    img = np.expand_dims(img, axis=0)
+    img = img / 255
+    answer = neuralNet.predict(img)
+    x, y = answer, np.argmax(answer)
+    eel.write(x, y)
+    print(x)
+    print(y)
+
+@eel.expose
+def learning():
     (train_images, train_answer), (test_images, test_answer) = mnist.load_data()
     train_images = train_images / 255
     test_images = test_images / 255
     train_answer = keras.utils.to_categorical(train_answer, 10)
     test_answer = keras.utils.to_categorical(test_answer, 10)
-
-    model.fit(train_images, train_answer, batch_size=40,
+    neuralNet.fit(train_images, train_answer, batch_size=40,
               epochs=1, validation_split=0.2)
-    model.evaluate(test_images, test_answer)
-
-
-@eel.expose
-def go():
-    img = format(eel.gray_format())
-    img = np.expand_dims(img, axis=0)
-    answer = neuralNet.predict(img)
-    x = np.argmax(answer)
-    return x
+    neuralNet.evaluate(test_images, test_answer)
 
 
 neuralNet = keras.Sequential([
@@ -47,9 +54,12 @@ neuralNet = keras.Sequential([
     Dense(100, activation='relu'),
     Dense(10, activation='softmax')
 ])
+print(neuralNet.summary())
 neuralNet.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
-eel.start('main.html', size=(830, 600), mode='chrome', port=8081)
+eel.start('main.html', size=(830, 550), mode='chrome', port=0)
+
+
 
