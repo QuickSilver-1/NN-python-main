b135061b3e1bf8b543cf1d9bbfa115366ce19034
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2022-03-08 04:06:09 +0500

    Update NN.py

--- NN.py
+++ NN.py
@@ -5,8 +5,9 @@ from keras.layers import Dense, Flatten
 from keras.datasets import mnist
 from PIL import Image
 import matplotlib.pyplot as plt
+from keras.models import load_model
 
-eel.init('D:/prog/indproject/Web')
+eel.init('web')
 
 
 @eel.expose
@@ -16,17 +17,17 @@ def go(data):
         pixels.shape = (392, 392)
         pixels = Image.fromarray(pixels)
         pixels = pixels.resize((28, 28), Image.ANTIALIAS)
-        pixels = np.array(pixels)
         return pixels
 
     img = formating(data)
     img = np.expand_dims(img, axis=0)
     img = img / 255
     answer = neuralNet.predict(img)
-    x, y = answer, np.argmax(answer)
-    eel.write(x, y)
+    x= answer
+    x = list(map(lambda x: round(x * 100, 2), x[0]))
     print(x)
-    print(y)
+    eel.write(x)
+
 
 @eel.expose
 def learning():
@@ -35,25 +36,26 @@ def learning():
     test_images = test_images / 255
     train_answer = keras.utils.to_categorical(train_answer, 10)
     test_answer = keras.utils.to_categorical(test_answer, 10)
-    neuralNet.fit(train_images, train_answer, batch_size=40,
-              epochs=5, validation_split=0.2)
+    neuralNet.fit(train_images, train_answer, batch_size=100,
+                  epochs=10, validation_split=0.2)
     neuralNet.evaluate(test_images, test_answer)
+    neuralNet.save('save.h5')
 
 
-neuralNet = keras.Sequential([
-    Flatten(input_shape=(28, 28, 1)),
-    Dense(100, activation='relu'),
-    Dense(10, activation='softmax')
-])
-print(neuralNet.summary())
-neuralNet.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
-eel.start('main.html', size=(830, 550), mode='chrome', port=0)
-
+@eel.expose
+def make_defoultnn(neirons):
+    global neuralNet
+    neuralNet = keras.Sequential([
+        Flatten(input_shape=(28, 28, 1)),
+        Dense(neirons, activation='relu'),
+        Dense(10, activation='softmax')])
+    neuralNet.compile(optimizer='adam',
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
 
 
+neuralNet = load_model('save.h5')
+eel.start('main.html', size=(830, 550), mode='chrome', port=8080, host='localhost')
 
 
 
