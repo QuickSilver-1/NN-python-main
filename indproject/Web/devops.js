eb10a66fff0011f4f3605af2571a8adb194876e0
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2022-03-09 00:21:18 +0500

    Update devops.js

--- indproject/Web/devops.js
+++ indproject/Web/devops.js
@@ -1,8 +1,23 @@
-async function train () {
-    
-    await eel.learning();
+const form = document.getElementById('model'),
+      training = document.getElementById('train');
+
+async function train (event) {
+    event.preventDefault();
+
+    const epochs = training.querySelector('[name="epochs"]').value;
+    await eel.learning(epochs);
 };
 
-function model () {
-    eel.make_defoultnn(512);
+function models (event) {
+    event.preventDefault();
+
+    const models = form.querySelector('[name="model"]').value,
+          neirons = form.querySelector('[name="many"]').value;
+    console.log(neirons);
+    if (models == 'perceptron') {
+        eel.make_defoultnn(neirons);
+    };
 };
+
+form.addEventListener('submit', models);
+training.addEventListener('submit', train);
